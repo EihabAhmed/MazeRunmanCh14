720185f0bcb64a950192eed72f5de61049ded831
EihabAhmed <eihabahmed@gmail.com>
2023-07-22 00:41:37 +0300

    Complete

---
+++ core/src/com/mygdx/mazerunmanch14/Coin.java
@@ -0,0 +1,12 @@
+package com.mygdx.mazerunmanch14;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class Coin extends BaseActor {
+    public Coin(float x, float y, Stage s) {
+        super(x, y, s);
+
+        loadTexture("coin.png");
+        setBoundaryPolygon(6);
+    }
+}
--- core/src/com/mygdx/mazerunmanch14/LevelScreen.java
+++ core/src/com/mygdx/mazerunmanch14/LevelScreen.java
@@ -1,7 +1,14 @@
 package com.mygdx.mazerunmanch14;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class LevelScreen extends BaseScreen {
@@ -10,6 +17,12 @@ public class LevelScreen extends BaseScreen {
     Hero hero;
 
     Ghost ghost;
+
+    Label coinsLabel;
+    Label messageLabel;
+
+    Sound coinSound;
+    Music windMusic;
 
     @Override
     public void initialize() {
@@ -28,6 +41,30 @@ public class LevelScreen extends BaseScreen {
 
         ghost = new Ghost(0, 0, mainStage);
         ghost.centerAtActor(maze.getRoom(11, 9));
+
+        for (BaseActor room : BaseActor.getList(mainStage, "com.mygdx.mazerunmanch14.Room")) {
+            Coin coin = new Coin(0, 0, mainStage);
+            coin.centerAtActor(room);
+        }
+
+        ghost.toFront();
+
+        coinsLabel = new Label("Coins left:", BaseGame.labelStyle);
+        coinsLabel.setColor(Color.GOLD);
+        messageLabel = new Label("...", BaseGame.labelStyle);
+        messageLabel.setFontScale(2);
+        messageLabel.setVisible(false);
+
+        uiTable.pad(10);
+        uiTable.add(coinsLabel);
+        uiTable.row();
+        uiTable.add(messageLabel).expandY();
+
+        coinSound = Gdx.audio.newSound(Gdx.files.internal("coin.wav"));
+        windMusic = Gdx.audio.newMusic(Gdx.files.internal("wind.mp3"));
+        windMusic.setLooping(true);
+        windMusic.setVolume(0.1f);
+        windMusic.play();
     }
 
     @Override
@@ -39,6 +76,43 @@ public class LevelScreen extends BaseScreen {
         if (ghost.getActions().size == 0) {
             maze.resetRooms();
             ghost.findPath(maze.getRoom(ghost), maze.getRoom(hero));
+        }
+
+        for (BaseActor coin : BaseActor.getList(mainStage, "com.mygdx.mazerunmanch14.Coin")) {
+            if (hero.overlaps(coin)) {
+                coin.remove();
+                coinSound.play(0.10f);
+            }
+        }
+
+        int coins = BaseActor.getList(mainStage, "com.mygdx.mazerunmanch14.Coin").size();
+        coinsLabel.setText("Coins left: " + coins);
+
+        if (coins == 0) {
+            ghost.remove();
+            ghost.setPosition(-1000, -1000);
+            ghost.clearActions();
+            ghost.addAction(Actions.forever(Actions.delay(1)));
+            messageLabel.setText("You win!");
+            messageLabel.setColor(Color.GREEN);
+            messageLabel.setVisible(true);
+        }
+
+        if (hero.overlaps(ghost)) {
+            hero.remove();
+            hero.setPosition(-1000, -1000);
+            ghost.clearActions();
+            ghost.addAction(Actions.forever(Actions.delay(1)));
+            messageLabel.setText("Game Over");
+            messageLabel.setColor(Color.RED);
+            messageLabel.setVisible(true);
+        }
+
+        if (!messageLabel.isVisible()) {
+            float distance = new Vector2(hero.getX() - ghost.getX(), hero.getY() - ghost.getY()).len();
+            float volume = -(distance - 64) / (300 - 64) + 1;
+            volume = MathUtils.clamp(volume, 0.10f, 1.00f);
+            windMusic.setVolume(volume);
         }
     }
 
